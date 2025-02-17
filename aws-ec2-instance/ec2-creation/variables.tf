c98aafbacb38ab97ff8464c7fc2a1c25cf866b8c
T-venkata <iamvenkata45@gmail.com>
2025-02-17 18:19:08 +0530

    Update variables.tf

--- aws-ec2-instance/ec2-creation/variables.tf
+++ aws-ec2-instance/ec2-creation/variables.tf
@@ -1,29 +1,29 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "ap-south-1"
+  default     = ""
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = ""
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c50b6f7dc3701ddd"
+  default     = ""
 }
 
 variable "instance_name" {
   description = "Tag for EC2 instance name"
   type        = string
-  default     = "Terraform-instance"
+  default     = ""
 }
 
 variable "key_name" {
   description = "Name of the SSH key pair"
   type        = string
-  default     = "my-keypair"
+  default     = ""
 }
